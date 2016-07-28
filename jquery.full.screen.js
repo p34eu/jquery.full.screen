73258e22d903e8801fab277e84f4e279c0993251
jq <p@p34.eu>
2016-07-28 23:36:30 +0300

    add timeouts to account transition time

--- jquery.full.screen.js
+++ jquery.full.screen.js
@@ -1,22 +1,22 @@
 /*
 *   https://github.com/p34eu/jquery.full.screen
 *   
- *   v0.0.8
+ *   v0.0.9
 *
 */
 
-(function($) {
+(function ($) {
     "use strict";
-    $.fn.fullscreen = function(options) {
+    $.fn.fullscreen = function (options) {
         var settings = $.extend({
             iconEnter: 'zmdi zmdi-fullscreen-alt',
             iconExit: 'zmdi zmdi-fullscreen-exit',
-            onenter: function() {},
-            onexit: function() {}
+            onenter: function () { },
+            onexit: function () { }
         }, options);
-        return this.each(function() {
+        return this.each(function () {
             var btn = $(this);
-            btn.on('click', function(e) {
+            btn.on('click', function (e) {
                 var target = $(this).data('target'),
                     t = !target ? document.documentElement : $(target).get()[0],
                     i = btn.children('i').first();
@@ -37,14 +37,16 @@
                 document.webkitExitFullscreen();
             }
             if (window.fulscr) {
-                delete(window.fulscr);
-                settings.onexit.call();
-                i.removeClass(settings.iconExit).addClass(settings.iconEnter);
+                delete (window.fulscr);
+                setTimeout(function () {
+                    settings.onexit.call();
+                    i.removeClass(settings.iconExit).addClass(settings.iconEnter);
+                }, 800);
             }
         }
 
         function enter(element, icon) {
-           
+
             if (element.requestFullscreen) {
                 element.requestFullscreen();
             } else if (element.mozRequestFullScreen) {
@@ -58,9 +60,9 @@
                 icon.removeClass(settings.iconEnter).addClass(settings.iconExit);
             }
             window.fulscr = 1;
-            setTimeout(function() {
-                 settings.onenter.call();
-                $(document).one('webkitfullscreenchange mozfullscreenchange fullscreenchange MSFullscreenChange', function(e) {
+            setTimeout(function () {
+                settings.onenter.call();
+                $(document).one('webkitfullscreenchange mozfullscreenchange fullscreenchange MSFullscreenChange', function (e) {
                     exit(element, icon);
                 });
             }, 800);
